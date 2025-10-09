da35286189b08d393132846cd9544f94590c8998
daniel1830s <115768962+daniel1830s@users.noreply.github.com>
2025-10-09 11:27:09 -0400

    Added initial backend support and functionality for registration/logging in

---
+++ backend/models/User.js
@@ -0,0 +1,31 @@
+// Defining the user model and schema
+import mongoose from "mongoose";
+import bcrypt from 'bcryptjs'
+
+const userSchema = new mongoose.Schema({
+    // mongoose creates an _id field by default, which will be the PK
+    name: { type: String, required: true},
+    email: { type: String, required: true, unique: true},
+    password: { type: String, required: true, select: false } // <-- don't allow password to be selected
+});
+
+// Hashing password
+userSchema.pre('save', async function(next) {
+    // if the password isn't changed, we don't need to re-save it
+    if (!this.isModified('password')) {
+        next();
+    }
+    // generate salt to ensure even if two passwords are the same,
+    // they result in different hashes
+    const salt = bcrypt.getSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+});
+
+// Compare passwords for authentication
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    // Compare the password entered to the one stored in the database
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
