281d79bf229a471677f320496dfeae33440c3608
Vaibhav Ganeriwala <vaibhavganeriwala@mac.mynetworksettings.com>
2025-10-14 04:00:42 -0400

    some small changes to backend stuff

--- backend/models/AdherenceLog.js
+++ backend/models/AdherenceLog.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const adherenceLogSchema = new mongoose.Schema({
-    atient: {
+    patient: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true,
