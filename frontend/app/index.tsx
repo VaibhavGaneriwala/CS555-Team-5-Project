1c3bd931268dce87b5db97a855396c7fb972e0fe
Jared Simonetti <jaredsimonetti2@gmail.com>
2025-10-14 11:06:46 -0400

    ADD: Init tailwinf css (frontend)

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -3,13 +3,16 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
+      className="flex-1 items-center justify-center bg-blue-500"  
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-2xl font-bold text-white">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
