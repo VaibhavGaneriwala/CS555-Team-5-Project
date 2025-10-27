7de4b178f6c721911e1c85a0f389ad6ee503ed4b
daniel1830s <115768962+daniel1830s@users.noreply.github.com>
2025-10-27 15:17:39 -0400

    Changed server.js - was defaulting to default route no matter which endpoint was hit. Additionally, fixed authorization for medicationController.

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,18 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+// Auth, medications, adherence routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/medications', require('./routes/medications'));
+app.use('/api/adherence', require('./routes/adherence'));
+
+const port = process.env.PORT
+
+// Default to home route
 app.use('/', (req, res) => {
     res.json({message: 'Medication Tracker API is running'});
 });
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/medications', require('./routes/medications'));
-app.use('/api/adherence', require('./routes/adherence'));
-const port = process.env.PORT
 app.listen(port, () => {
     console.log("Server is running on port 3000");
 });
